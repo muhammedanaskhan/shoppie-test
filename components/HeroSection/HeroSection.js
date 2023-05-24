b140cbf65fa6c9c9313af3b5389ece50207a2dfa
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-05-24 12:21:06 -0700

    banner image fixed

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import styles from "/components/HeroSection/HeroSection.module.css"
 function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <div className={styles.heroContainer}>
+ 
+
+    </div>
   )
 }
 
 export default HeroSection
+
+
+// https://huckberry.imgix.net/pages/3-0_featured_365shorts_2303-7b9c3911e0.jpg?auto=format%2C%20compress&amp;crop=top&amp;fit=clip&amp;cs=tinysrgb&amp;ixlib=react-9.5.2
